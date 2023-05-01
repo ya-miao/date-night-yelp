127473564be44b18c6cdf81a0aba26f2e156b3f6
tril-studio <127056177+tril-studio@users.noreply.github.com>
2023-05-01 13:10:24 -0400

    Base app for couple yelp

---
+++ src/pages/CoupleYelp.js
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const CoupleYelp = () => {
+  return (
+    <Box>
+      <Typography>Component - CoupleYelp</Typography>
+    </Box>
+  )
+};
+
+export default CoupleYelp;
