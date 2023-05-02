4f83c59f325e8be04f84caee2378f1892f2b6ff1
tril-studio <127056177+tril-studio@users.noreply.github.com>
2023-05-02 10:26:17 -0400

    Implemented example working Yelp API call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+
+import axios from 'axios'
 
 import { Container } from '@mui/material';
 
 import CoupleYelp from './pages/CoupleYelp';
 
 const App = () => {
+
+  // Example working API call. We can work off of this base.
+  const config = {
+    headers: {
+      Authorization:
+        "Bearer KNMAWkmyDKUpkTY7xuFS4bXpVgWS9uunqKuhBlfEw9mn4BOjrfNl1nDeSJphPP9LIsbGXMWCjjBX1S3EJtuhR4ackXwAB5Re_A2O0ZYP1lNFQ-SQJ5l2gjVCsTFQZHYx",
+    },
+    params: {
+      term: "restaurants",
+      location: 'NYC',
+      sort_by: "best_match",
+      limit: 50,
+    },
+  };
+  //
+  axios
+    .get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search`, config)
+    .then((response) => {
+      console.log('response: ');
+      console.log(response);
+    });
+  //
+
   return (
     <Container sx={{ my: 4 }}>
       <CoupleYelp />
