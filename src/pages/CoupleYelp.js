c7cbb1aa289453dbe36cc36657b06c8c3b733b2d
Anthony Sanchez <antsanchez198@gmail.com>
2023-05-04 17:09:49 -0400

    some design changes

--- src/pages/CoupleYelp.js
+++ src/pages/CoupleYelp.js
@@ -6,6 +6,7 @@ import DiningSvg from "../images/dining.svg";
 import PreferenceCard from "../components/PreferenceCard";
 import { orange, yellow } from '@mui/material/colors';
 import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
+import PinDropIcon from '@mui/icons-material/PinDrop';
 import ResultsDialog from '../components/ResultsDialog';
 
 // Let's add some way for users to input person 1 and person 2
@@ -28,22 +29,23 @@ const CoupleYelp = ({ callYelpApi, restaurantResults, setMaxDistance, setCategor
   return (
     <Stack mb={4}>
       <Stack direction='row' justifyContent='space-between' sx={{ backgroundColor: "#28282B", padding: "0.5rem 2rem"}}>
-        <Stack direction='row' alignItems='center' justifyContent='center' gap={1}>
-          <Typography style={{color: "white"}}>Current Location: <span style={{color: "orange"}}>{location.principalSubdivision}</span></Typography>
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="15" height="15" viewBox="0 0 24 24" stroke-width="1.5" stroke={orange[500]} fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <Stack direction='row' alignItems='center' justifyContent='center' gap={0.5}>
+          <Typography style={{color: "white", fontWeight: "bold"}}>Current Location: <span style={{color: "orange"}}>{location.principalSubdivision}</span></Typography>
+          {/* <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="15" height="15" viewBox="0 0 24 24" stroke-width="1.5" stroke={orange[500]} fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <circle cx="12" cy="11" r="3" />
             <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
-          </svg>
+          </svg> */}
+          <PinDropIcon size='small' style={{color: "orange"}}/>
         </Stack>
         <button onClick={signOut} className='signout-btn'
         >Sign Out</button>
       </Stack>
       <Stack alignItems='center' justifyContent='center' mt={2}>
         <img src={DiningSvg} alt="Your SVG" className='dining-image' />
-        <Stack direction="column" mt={2} sx={{ width: "fitContent", height: "95%", borderRadius: "5px" }} padding={4} bgcolor="white" alignItems="center" alignContent="center">
-            <Typography variant="h3" fontFamily="Pacifico">Not sure where to eat?</Typography>
-          <Box direction="row" mt={3}>
+        <Stack direction="column" mt={2} className='white-container' padding={4} bgcolor="white" alignItems="center" alignContent="center">
+            <Typography variant="h3" fontFamily="Pacifico" className="underlined">Not sure where to eat?</Typography>
+          <Box direction="row" mt={5}>
             <Grid container spacing={3}>
               <Grid item xs={6}>
                 <PreferenceCard oneUser='Person One' mainColor="#A64AC9" secColor="#eecdfb"
