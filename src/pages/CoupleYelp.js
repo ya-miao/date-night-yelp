a69c6938bdc4801410a223a4953631f9310d43b9
Anthony Sanchez <antsanchez198@gmail.com>
2023-05-04 22:19:24 -0400

    input name feature

--- src/pages/CoupleYelp.js
+++ src/pages/CoupleYelp.js
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 
 import { Auth } from 'aws-amplify';
-import { Box, Button, Divider, Grid, Stack, Typography } from "@mui/material";
+import { Box, Button, Divider, Grid, Stack, Typography, TextField } from "@mui/material";
 import DiningSvg from "../images/dining.svg";
 import PreferenceCard from "../components/PreferenceCard";
-import { orange, yellow } from '@mui/material/colors';
-import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import PinDropIcon from '@mui/icons-material/PinDrop';
 import ResultsDialog from '../components/ResultsDialog';
 
@@ -21,17 +24,40 @@ const CoupleYelp = ({ callYelpApi, restaurantResults, setCategory, setMaxDistanc
   }
 
   const [openResults, setOpenResults] = useState(false);
+  const [user1Name, setUser1Name] = useState("");
+  const [user2Name, setUser2Name] = useState("");
+  const [open, setOpen] = useState(true);
 
   const handleCloseResults = () => {
     setOpenResults(false);
   };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Stack mb={4}>
-      <Stack direction='row' justifyContent='space-between' sx={{ backgroundColor: "#28282B", padding: "0.5rem 2rem"}}>
+      <Dialog
+        fullWidth="sm"
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>Please input your names</DialogTitle>
+        <DialogContent>
+          <Stack spacing={10} direction="row">
+            <TextField id="standard-basic" label="Person 1" variant="standard" onChange={e => setUser1Name(e.target.value)}/>
+            <TextField id="standard-basic" label="Person 2" variant="standard" onChange={e => setUser2Name(e.target.value)}/>
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+      <Stack direction='row' justifyContent='space-between' sx={{ backgroundColor: "#28282B", padding: "0.5rem 2rem" }}>
         <Stack direction='row' alignItems='center' justifyContent='center' gap={0.5}>
-          <Typography style={{color: "white", fontWeight: "bold"}}>Current Location: <span style={{color: "orange"}}>{location.locality + `, ` + location.principalSubdivision}</span></Typography>
-          <PinDropIcon size='small' style={{color: "orange"}}/>
+          <Typography style={{ color: "white", fontWeight: "bold" }}>Current Location: <span style={{ color: "orange" }}>{location.locality + `, ` + location.principalSubdivision}</span></Typography>
+          <PinDropIcon size='small' style={{ color: "orange" }} />
         </Stack>
         <button onClick={signOut} className='signout-btn'
         >Sign Out</button>
@@ -39,18 +65,18 @@ const CoupleYelp = ({ callYelpApi, restaurantResults, setCategory, setMaxDistanc
       <Stack alignItems='center' justifyContent='center' mt={2}>
         <img src={DiningSvg} alt="Your SVG" className='dining-image' />
         <Stack direction="column" mt={2} className='white-container' padding={4} bgcolor="white" alignItems="center" alignContent="center">
-            <Typography variant="h3" fontFamily="Pacifico" className="underlined">Not sure where to eat?</Typography>
+          <Typography variant="h3" fontFamily="Pacifico" className="underlined">Not sure where to eat?</Typography>
           <Box direction="row" mt={5}>
             <Grid container spacing={3}>
               <Grid item xs={6}>
-                <PreferenceCard oneUser='Person One' mainColor="#A64AC9" secColor="#eecdfb"
+                <PreferenceCard oneUser={user1Name} mainColor="#A64AC9" secColor="#eecdfb"
                   setCategory={setCategory}
                   setMaxDistance={setMaxDistanceOne}
                   setPriceLevel={setPriceLevelOne} />
               </Grid>
               <Divider></Divider>
               <Grid item xs={6}>
-                <PreferenceCard oneUser='Person Two' mainColor="#473890" secColor="#d7cffc"
+                <PreferenceCard oneUser={user2Name} mainColor="#473890" secColor="#d7cffc"
                   setCategory={setCategory}
                   setMaxDistance={setMaxDistanceTwo}
                   setPriceLevel={setPriceLevelTwo} />
